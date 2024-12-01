6c0394ee6b4a89e0fc617fdd839dae6991f0542c
= <message4dylan@gmail.com>
2024-12-01 12:12:54 +0000

    part b

--- 2024/1/src/main.rs
+++ 2024/1/src/main.rs
@@ -1,7 +1,12 @@
 use std::{fs::read_to_string, iter::zip};
+use itertools::Itertools;
 
 fn main() {
-    let (mut left, mut right): (Vec<i32>, Vec<i32>) = read_to_string("input.txt")
+    println!("{}", sum_of_similarity_scores("input.txt"));
+}
+
+fn sum_of_sorted_differences(path: &str) -> i32 {
+    let (mut left, mut right): (Vec<i32>, Vec<i32>) = read_to_string(path)
         .unwrap()
         .lines()
         .map(|line| {
@@ -15,14 +20,38 @@ fn main() {
     right.sort();
 
 
-    let result = zip(left, right)
+    zip(left, right)
         .map(|(l, r)| {
             (l - r).abs()
         })
         .reduce(|running_total: i32, new| {
             running_total + new
         })
-        .unwrap();
+        .unwrap()
+}
 
-    println!("{}", result);
+fn sum_of_similarity_scores(path: &str) -> i32 {
+    let (left, right): (Vec<i32>, Vec<i32>) = read_to_string(path)
+        .unwrap()
+        .lines()
+        .map(|line| {
+            let elements: Vec<&str> = line.split("   ").collect();
+            (elements[0].parse::<i32>().unwrap(),
+            elements[1].parse::<i32>().unwrap())
+        })
+        .unzip();
+
+//    left.sort();
+    let frequencies = right.into_iter().counts();
+
+
+    left
+        .into_iter()
+        .map(|l| {
+            l * *frequencies.get(&l).unwrap_or(&0) as i32
+        })
+        .reduce(|running_total: i32, new| {
+            running_total + new
+        })
+        .unwrap()
 }
