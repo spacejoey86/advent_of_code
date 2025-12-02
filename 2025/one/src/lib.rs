51213d9bdf8ec88f3807431fc2c48f8476349da6
Dylan Gardham <message4dylan@gmail.com>
2025-12-02 12:33:10 +0000

    attempted optimisations

--- 2025/one/src/lib.rs
+++ 2025/one/src/lib.rs
@@ -1,6 +1,4 @@
-use std::fs::read_to_string;
-
-pub fn part_one(input: &str) -> u32 {
+pub fn part_one(input: &str) -> i32 {
     let mut dial = Dial::new();
     for line in input.lines() {
         dial.rotate(Dial::parse_rotate(line))
@@ -8,7 +6,7 @@ pub fn part_one(input: &str) -> u32 {
     return dial.times_left_at_zero()
 }
 
-pub fn part_two(input: &str) -> u32 {
+pub fn part_two(input: &str) -> i32 {
     let mut dial = Dial::new();
     for line in input.lines() {
         dial.rotate_part2(Dial::parse_rotate(line))
@@ -18,10 +16,11 @@ pub fn part_two(input: &str) -> u32 {
 
 struct Dial {
     pointing: i32,
-    times_left_at_zero: u32,
+    times_left_at_zero: i32,
 }
 
 impl Dial {
+    pub const DIAL_SIZE: i32 = 100;
     pub fn new() -> Self {
         return Self {
             pointing: 50,
@@ -30,7 +29,7 @@ impl Dial {
     }
 
     fn rotate(&mut self, distance: i32) {
-        self.pointing = (self.pointing + distance) % 100;
+        self.pointing = (self.pointing + distance) % Dial::DIAL_SIZE;
         if self.pointing == 0 {
             self.times_left_at_zero += 1;
         }
@@ -43,16 +42,68 @@ impl Dial {
         }
     }
 
+    fn rotate_part2_fast(&mut self, distance: i32) {
+        // technically there should be an early return if distance=0, but that is never in the input
+        // because my logic would give the wrong output
+        while self.pointing >= Dial::DIAL_SIZE {
+            self.pointing -= Dial::DIAL_SIZE;
+            self.times_left_at_zero += 1;
+        }
+        if distance > 0 {
+            self.pointing += distance;
+            self.times_left_at_zero += self.pointing / Dial::DIAL_SIZE;
+        } else if distance < 0 {
+            self.pointing += distance;
+            self.times_left_at_zero += distance.abs() / -Dial::DIAL_SIZE;
+        }
+        self.pointing = self.pointing % Dial::DIAL_SIZE;
+        // self.pointing = (self.pointing + distance) % Dial::DIAL_SIZE;
+        // if self.pointing == 0 {
+        //     self.times_left_at_zero += 1;
+        // }
+    }
+
     pub fn parse_rotate(line: &str) -> i32 {
         let distance: i32 = line[1..].parse().unwrap();
         match line.chars().nth(0) {
             Some('L') => -distance,
             Some('R') => distance,
-            _ => panic!("failed to parse line: {}", line)
+            _ => unreachable!("failed to parse line: {}", line)
         }
     }
 
-    pub fn times_left_at_zero(&self) -> u32 {
+    pub fn times_left_at_zero(&self) -> i32 {
         return self.times_left_at_zero;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs::read_to_string;
+
+    use super::*;
+
+    #[test]
+    fn part_one_test() {
+        let input = read_to_string("input.txt").unwrap();
+        assert_eq!(part_one(&input), 1177)
+    }
+
+    #[test]
+    fn part_one_example_test() {
+        let input = read_to_string("test_input.txt").unwrap();
+        assert_eq!(part_one(&input), 3)
+    }
+
+    #[test]
+    fn part_two_test() {
+        let input = read_to_string("input.txt").unwrap();
+        assert_eq!(part_two(&input), 6768)
+    }
+
+    #[test]
+    fn part_two_example_test() {
+        let input = read_to_string("test_input.txt").unwrap();
+        assert_eq!(part_two(&input), 6)
     }
 }
