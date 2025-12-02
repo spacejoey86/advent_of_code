4be942cc5acaa5956ffbd6423afd1e21c06139d2
Dylan Gardham <message4dylan@gmail.com>
2025-12-02 14:20:19 +0000

    day 2 part 2

--- 2025/two/src/lib.rs
+++ 2025/two/src/lib.rs
@@ -1,9 +1,5 @@
 use std::ops::RangeInclusive;
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
 pub fn part_one(input: &str) -> u64 {
     return id_iterator(input).filter(|id| id_invalid(*id)).sum();
 }
@@ -21,6 +17,29 @@ fn id_invalid(id: u64) -> bool {
         return false;
     }
     id % (10_u64.pow(num_digits / 2) + 1) == 0
+}
+
+pub fn part_two(input: &str) -> u64 {
+    return id_iterator(input).filter(|id| p2_id_invalid(*id)).sum();
+}
+
+fn p2_id_invalid(id: u64) -> bool {
+    let string_id = id.to_string();
+    for i in 1..string_id.len() {
+        let prefix = &string_id[..i];
+        if is_prefix(prefix, &string_id[i..]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+fn is_prefix(prefix: &str, suffix: &str) -> bool {
+    let mut repeated = prefix.to_string();
+    while repeated.len() < suffix.len() {
+        repeated.push_str(prefix);
+    }
+    return repeated == suffix;
 }
 
 #[cfg(test)]
@@ -43,5 +62,37 @@ mod tests {
     fn part_one_test() {
         let input = read_to_string("input.txt").unwrap();
         assert_eq!(part_one(&input), 37314786486)
+    }
+
+    #[test]
+    fn prefix() {
+        assert!(is_prefix("test", "test"))
+    }
+
+    #[test]
+    fn double_prefix() {
+        assert!(is_prefix("oh", "ohoh"))
+    }
+
+    #[test]
+    fn not_prefix() {
+        assert!(!is_prefix("t", "to"))
+    }
+
+    #[test]
+    fn p2_id() {
+        assert!(p2_id_invalid(121212))
+    }
+
+    #[test]
+    fn part_two_example_test() {
+        let input = read_to_string("test_input.txt").unwrap();
+        assert_eq!(part_two(&input), 4174379265)
+    }
+
+    #[test]
+    fn part_two_test() {
+        let input = read_to_string("input.txt").unwrap();
+        assert_eq!(part_two(&input), 47477053982)
     }
 }
